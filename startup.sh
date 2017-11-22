8749d14ced9cd5438ef7a1dd518cc601130d2212
vkargov <kargov@gmail.com>
2017-11-21 18:32:38 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
+# TODO
+# For whatever reason it doesn't work correctly as xinitrc.
+# I need to investigate the cause and merge the 2 files together.
 xmodmap ~/.xmodmap && xbindkeys && xcape -e 'Control_R=Return'
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Finger" 50 90 255
