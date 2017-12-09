94644855ab48a5baa0686c718de42fb5c25b4d59
undercat <Vladimir Kargov>
2017-12-08 21:22:20 -0800

    [xinit] Prevent the touchpad driver from following my life's mistakes.

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 # For whatever reason it doesn't work correctly as xinitrc.
 # I need to investigate the cause and merge the 2 files together.
 xmodmap ~/.xmodmap && xbindkeys && xcape -e 'Control_R=Return'
-xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Finger" 50 90 255
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Finger" 50 90 255 # After some testing, this seems to make no difference.
+xinput set-prop 13 "Synaptics Noise Cancellation" 15 15 # This seems to be the right value
