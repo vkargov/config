4d6663893cb6143c228373fcb8186cc6e1d78cfd
undercat <Vladimir Kargov>
2017-12-09 22:46:43 -0800

    [startup] This is starting to get out of hand.

--- startup.sh
+++ startup.sh
@@ -4,4 +4,7 @@
 xmodmap ~/.xmodmap && xbindkeys && xcape -e 'Control_R=Return'
 xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Finger" 50 90 255 # After some testing, this seems to make no difference.
 xinput set-prop 13 "Synaptics Noise Cancellation" 15 15 # This seems to be the right value
-nh bash -c 'while true; do date; mplayer --really-quiet ~/derp/bell/bells/thinhChuong.wav; sleep 420; done'
+
+# Mindfulness bell thing
+nh bash -c 'i=0; while true; do date; if [[ $i -eq 0 ]]; then notify-send "$(shuf -n 1 ~/derp/bell/quotes.txt)"; fi; mplayer --really-quiet ~/derp/bell/bells/thinhChuong.wav; sleep 300; i=$((($i+1)%3)); done'
+
