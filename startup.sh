2581ef2ceb36d00ad24ba3ec1c83e44008d9f467
Vladimir Kargov <kargov@gmail.com>
2018-01-16 12:37:08 -0800

    [bell] trying out mpv

--- startup.sh
+++ startup.sh
@@ -6,4 +6,4 @@ xinput set-prop "SynPS/2 Synaptics TouchPad" "Synaptics Finger" 50 90 255 # Afte
 xinput set-prop 13 "Synaptics Noise Cancellation" 15 15 # This seems to be the right value
 
 # Mindfulness bell thing
-nh bash -c 'i=0; while true; do date; if [[ $i -eq 0 ]]; then notify-send "$(cat ~/derp/bell/quotes.txt | grep -o "^[^#]*" | shuf -n 1)"; fi; mplayer --really-quiet ~/derp/bell/bells/thinhChuong.wav; sleep 300; i=$((($i+1)%3)); done'
+n bash -c 'i=0; while true; do date; if [[ $i -eq 0 ]]; then notify-send "$(cat ~/derp/bell/quotes.txt | grep -o "^[^#]*" | shuf -n 1)"; fi; mpv --really-quiet ~/derp/bell/bells/thinhChuong.wav; sleep 300; i=$((($i+1)%3)); done'
