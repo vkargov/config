534c6349542b100621afe662d9d1a21a9ac7e4aa
Vladimir Kargov <kargov@gmail.com>
2019-11-27 08:22:18 +0300

    [wakeup] disable light on razer mouse

--- wakeup.sh
+++ wakeup.sh
@@ -2,3 +2,7 @@ xsetwacom set "Wacom Intuos S 2 Pad pad" Button 3 key "F9" # drawning/cursor swi
 xsetwacom set "Wacom Intuos S 2 Pad pad" Button 1 key "Shift" # Eraser
 xsetwacom set "Wacom Intuos S 2 Pad pad" Button 9 key "Shift F9" # Clear
 xsetwacom set "Wacom Intuos S 2 Pad pad" Button 8 key "F10" # Undo
+sudo rivalcfg -b os -c 000000
+# (while true; do color=`printf '#%.2x%.2x%.2x' $((RANDOM%255)) $((RANDOM%255)) $((RANDOM%255))`; echo $color; sudo rivalcfg -b os -c $color; read; done)
+# for color exploration
+# 050101 is dark pink-ish, but it sucks
